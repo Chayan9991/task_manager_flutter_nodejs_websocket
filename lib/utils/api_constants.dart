ec83ff086a2f04a02cfee41cac1c209141f9ddc0
Chayan Barman <barmanchayan10@gmail.com>
2025-05-20 19:49:22 +0530

    fixed api constraints

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,4 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'https://task-manager-server.onrender.com/api';
-  static const String socketUrl = 'https://task-manager-server.onrender.com';
+  static const String baseUrl =
+      'https://task-manager-flutter-nodejs-websocket.onrender.com/api';
+  static const String socketUrl =
+      'https://task-manager-flutter-nodejs-websocket.onrender.com';
 }
